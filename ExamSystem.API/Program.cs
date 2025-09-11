eaeefa295a148cccc2099cdaa6d8417c8d2da4c9
Eyyub Qədirov <88050140+QEyyub@users.noreply.github.com>
2025-09-11 09:34:22 +0400

    Update Program.cs

--- ExamSystem.API/Program.cs
+++ ExamSystem.API/Program.cs
@@ -13,7 +13,7 @@ builder.Services.AddMediatR(cfg =>
 });
 
 builder.Services.AddInfrastructure(builder.Configuration);
-
+//data
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowAngularDevClient", policy =>
